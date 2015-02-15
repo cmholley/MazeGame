39616d503a1ed2a9bd97e782d022311db52e3367
Chris Holley <cmholley97@gmail.com>
2015-02-14 18:51:08 -0600

    no message

---
+++ Assets/RandomMapGeneration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMapGeneration : MonoBehaviour {
+
+    public GameObject prefab;
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float spacing = 2f;
+
+
+	// Use this for initialization
+    void Start(){
+        for (int y = 0; y < gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                Vector3 pos = new Vector3(x, 0, y) * spacing;
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
