95783554290d2036a5e14790f8ca93db2608b486
Erin Dunnahoo <ccshorts@yahoo.com>
2015-02-15 09:36:51 -0600

    no message

---
+++ Assets/OpenTreasureChest.js
@@ -0,0 +1,9 @@
+﻿var door : GameObject; private var HasBeenTriggered : int;
+
+HasBeenTriggered = 0;
+
+function OnTriggerEnter (collision : Collider) 
+{ if (collision.gameObject.tag == "Door") 
+	{ if(HasBeenTriggered == 0) 
+		{ door.animation.Play ();
+		 HasBeenTriggered = 1; } } }
